819539400d2fd54426a2957bb3f1adc95e7b79fb
aman-maharshi <amanm4harshi@gmail.com>
2024-12-08 01:28:37 +0530

    header minor chagnes

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -43,6 +43,24 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                // className={clsx("relative z-2 transition-all duration-500 delay-300", isOpen ? "translate-y-0" : "translate-y-[500px]")}
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
 
