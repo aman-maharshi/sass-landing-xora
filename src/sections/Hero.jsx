53f08ec05024146f9501b88cb5aad3ec8a4df854
aman-maharshi <amanm4harshi@gmail.com>
2024-12-09 02:00:40 +0530

    hero section updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,6 +23,14 @@ const Hero = () => {
               <Button icon="/images/zap.svg">Try it now</Button>
             </LinkScroll>
           </div>
+
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt="hero"
+            />
+          </div>
         </div>
       </Element>
     </div>
